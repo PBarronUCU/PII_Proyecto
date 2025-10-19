5c89f6d072abea516c023b874f95504c399fcdf6
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-10-19 14:25:23 -0300

    Agrego Constructor de Cliente.cs Ya chekeando que no se repita las clabes

--- src/Library/Cliente.cs
+++ src/Library/Cliente.cs
@@ -1,10 +1,32 @@
+using System;
+
 namespace Library
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 {
     public class Cliente
     {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public string Correo { get; }
         public int Tel { get; }
+        public string Genero { get; }
+        public DateTime FechaNac { get; set; }
+
+        public Cliente(string nombre, string apellido, string correo, int tel)
+        {
+            BaseDatos bd1 = BaseDatos.Instance;
+            if (!bd1.ExisteCorreo(correo) && !bd1.ExisteTel(tel))
+            {
+                Nombre = nombre;
+                Apellido = apellido;
+                Correo = correo;
+                Tel = tel;
+            }
+            else
+            {
+                Console.WriteLine("Correo o Telefono ya ocupado");
+            }
+        }
     }
 }
