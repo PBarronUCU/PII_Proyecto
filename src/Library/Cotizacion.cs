c4d8ccaf3340ae3c3ec8a9d84856b62c32a3ed2f
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-10-19 15:46:14 -0300

    Implemento constructor de Cotizacion.cs y mas metodos de Usuario.cs

--- src/Library/Cotizacion.cs
+++ src/Library/Cotizacion.cs
@@ -1,9 +1,26 @@
+using System;
+
 namespace Library
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 {
     public class Cotizacion
     {
+        public Usuario Usuario { get;}
+        public int ClienteTel { get; }
+        public DateTime Fecha { get; set; }
+        public Double Valor { get; set; }
+        public String Importancia { get; set; }
+
+        public Cotizacion(Usuario usuario, int clienteTel, DateTime fecha, Double valor, String imp)
+        {
+            Usuario = usuario;
+            ClienteTel = clienteTel;
+            Fecha = fecha;
+            Valor = valor;
+            Importancia = imp;
+            
+        }
         
     }
 }
--- src/Library/Usuario.cs
+++ src/Library/Usuario.cs
@@ -12,6 +12,7 @@ namespace Library
         public string Apellido { get; set; }
         public string Correo { get; }
         public List<Cliente> Cartera = new List<Cliente>();
+        public List<Cotizacion> OportunidadesVentas = new List<Cotizacion>();
         public List<IInteracion> ListaInteracciones = new List<IInteracion>();
         public bool Suspendido { get; }
 
@@ -31,5 +32,54 @@ namespace Library
                 Console.WriteLine("El correo ya esta ocupado");
             }
         }
+
+        public void AgregarCliente(String name, String apell, int tel, String correo)
+        {
+            BaseDatos bd1 = BaseDatos.Instance; 
+            //Inecesario, pero por si acaso para no encontrarme errores al agregarlo a la cartera
+            if (!bd1.ExisteCorreo(correo) && !bd1.ExisteTel(tel))
+            {
+                Cliente instanca1 = new Cliente(name, apell, correo, tel);
+                Cartera.Add(instanca1);
+            }
+            else
+            {
+                Console.WriteLine("El correo o telefono ya esta ocupado");
+            }
+        }
+        
+        public void CrearVentas()
+        {
+            //Falta Implementar Ventas
+        }
+        
+        //Vere si la agrego al UML
+        public bool VerificarTelCartera(int tel)
+        {
+            bool resultado = false;
+            foreach (Cliente cliente in Cartera)
+            {
+                if (cliente.Tel == tel)
+                {
+                    resultado = true;
+                }
+            }
+            return resultado;
+        }
+
+        public void CrearCoti(int telcliente, DateTime fecha, Double valor, String imp)
+        {
+            if (VerificarTelCartera(telcliente))
+            {
+                Cotizacion instancia2 = new Cotizacion(this,telcliente,fecha, valor, imp);
+                OportunidadesVentas.Add(instancia2);
+            }
+            else
+            {
+                Console.WriteLine("Telefono no encontrado");
+            }
+        }
+        
+        
     }
 } 
