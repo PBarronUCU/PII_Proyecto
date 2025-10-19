67f1041f257bffa6c3f5d14d866e2d5ad7ef65c5
Giuseppe Gasparri Silveira <GIUSEPPE.GASPARRI@correo.ucu.edu.uy>
2025-10-19 17:46:38 -0300

    Implemento clase ventas

--- src/Library/Ventas.cs
+++ src/Library/Ventas.cs
@@ -5,6 +5,17 @@ namespace Library
 {
     public class Ventas
     {
-        
+        public Usuario Usuario { get; set; }
+        public int ClienteTel { get; set; }
+        public string Producto { get; set; }
+        public double Precio { get; set; }
+
+        public Ventas(Usuario usuario, int clienteTel, string producto, double precio)
+        {
+            Usuario = usuario;
+            ClienteTel = clienteTel;
+            Producto = producto;
+            Precio = precio;
+        }
     }
 }
