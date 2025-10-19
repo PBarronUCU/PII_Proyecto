3712058a12e370c4caacb5fb36cbc57c0f1e746f
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-10-19 14:03:47 -0300

    Agrego metodos a BD

--- src/Library/BaseDatos.cs
+++ src/Library/BaseDatos.cs
@@ -20,6 +20,72 @@ namespace Library
         {
             get { return _instance; }
         }
+
+        public bool ExisteCorreo(string correo)
+        {
+            bool result = false;
+            foreach (Usuario user in ListaUsuario)
+            {
+                if (user.Correo == correo)
+                {
+                    result = true;
+                }
+            }
+            foreach (Cliente client in ListaCliente)
+            {
+                if (client.Correo == correo)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        
+        public bool ExisteTel(int tel)
+        {
+            bool result = false;
+            foreach (Cliente client in ListaCliente)
+            {
+                if (client.Tel == tel)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public bool ExisteCorreoUser(string correo)
+        {
+            bool result = false;
+            foreach (Usuario user in ListaUsuario)
+            {
+                if (user.Correo == correo)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public void AgregarUsuario(Usuario user)
+        {
+            string correo = user.Correo;
+            if (!ExisteCorreo(correo))
+            {
+                ListaUsuario.Add(user);
+            }
+            
+        }
+
+        public void AgregarCliente(Cliente client)
+        {
+            string correo = client.Correo;
+            int tel = client.Tel;
+            if (!ExisteCorreo(correo) && !ExisteTel(tel))
+            {
+                ListaCliente.Add(client);
+            }
+        }
         
     }
 }
