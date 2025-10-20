3459103924541fd33825a216bc137305488938a2
Giuseppe Gasparri Silveira <GIUSEPPE.GASPARRI@correo.ucu.edu.uy>
2025-10-20 08:19:04 -0300

    Fecha de las Ventas

--- src/Library/Ventas.cs
+++ src/Library/Ventas.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 
@@ -5,17 +9,19 @@ namespace Library
 {
     public class Ventas
     {
+        public DateTime Dia_venta { get; set; }
         public Usuario Usuario { get; set; }
         public int ClienteTel { get; set; }
         public string Producto { get; set; }
         public double Precio { get; set; }
 
-        public Ventas(Usuario usuario, int clienteTel, string producto, double precio)
+        public Ventas(Usuario usuario, int clienteTel, string producto, double precio, DateTime diaVenta)
         {
             Usuario = usuario;
             ClienteTel = clienteTel;
             Producto = producto;
             Precio = precio;
+            Dia_venta = diaVenta;
         }
     }
 }
