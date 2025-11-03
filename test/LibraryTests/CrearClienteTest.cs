77a199f7eb6e569f8c31fa2d5428c136eb0a5da3
Rodrigo Montiel <rodrigo.montiel@correo.ucu.edu.uy>
2025-11-03 10:20:39 -0300

    Arreglo tests

--- test/LibraryTests/CrearClienteTest.cs
+++ test/LibraryTests/CrearClienteTest.cs
@@ -10,19 +10,20 @@ namespace LibraryTests
         [Test]
         public void CrearUsuario_ConNombreYApellidoValidos_NoLanzaExcepcion()
         {
-            var usuario = new Usuario("", "");
+            var usuario = new Usuario("", "", "");
 
             Assert.That(usuario.Nombre, Is.EqualTo("Juan"));
             Assert.That(usuario.Apellido, Is.EqualTo("Pérez"));
+            Assert.That(usuario.Correo,Is.EqualTo("JuanPerez@gmail.com"));
         }
 
-        [TestCase("", "Pérez")]
-        [TestCase("Juan", "")]
-        [TestCase(" ", "Gómez")]
-        [TestCase("Carlos", " ")]
-        public void CrearUsuario_ConNombreOVacio_DeberiaLanzarExcepcion(string nombre, string apellido)
+        [TestCase("", "Pérez", " ")]
+        [TestCase("Juan", "", " ")]
+        [TestCase(" ", "Gómez", " ")]
+        [TestCase("Carlos", " ", " ")]
+        public void CrearUsuario_ConNombreOVacio_DeberiaLanzarExcepcion(string nombre, string apellido, string correo)
         {
-            Assert.Throws<ArgumentException>(() => new Usuario(nombre, apellido));
+            Assert.Throws<ArgumentException>(() => new Usuario(nombre, apellido, correo));
         }
     }
 }
